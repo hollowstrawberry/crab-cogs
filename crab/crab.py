11f35a4ef2d601187810058f776cef5d74bda5ae
Samuel <33796679+orchidalloy@users.noreply.github.com>
2022-10-17 05:01:27 -0300

    Add draw/paint command

--- crab/crab.py
+++ crab/crab.py
@@ -10,8 +10,6 @@ from redbot.core.data_manager import cog_data_path
 from typing import *
 
 DONUT_FILE = "donuts.json"
-IMG_DL = "download.png"
-IMG_OUT = "output.jpg"
 
 class Crab(commands.Cog):
     """Random fun commands for the crab friend group."""
@@ -108,26 +106,58 @@ class Crab(commands.Cog):
         await self.config.donuts.set(emojis)
         await ctx.react_quietly("✅")
 
-    @commands.command(aliases=["paintme", "paint", "drawme"])
+    @commands.command(aliases=["drawme", "sketch", "sketchme"])
+    @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
     async def draw(self, ctx: commands.Context, user: Union[discord.User, str] = None):
-        """Produces a painting of you or someone else"""
+        """Produces a pencil drawing of you or someone else"""
         if user == "me" or user is None:
             user = ctx.author
         elif user == "you" or user == "yourself":
             user = self.bot.user
         elif isinstance(user, str):
-            return await ctx.send("who?")
+            return await ctx.send("Who?")
+        await ctx.trigger_typing()
         # load image
-        await user.avatar_url.save(cog_data_path(self).joinpath(IMG_DL))
-        Image.open(cog_data_path(self).joinpath(IMG_DL)).convert('RGB').resize((256, 256), Image.BICUBIC).save(cog_data_path(self).joinpath(IMG_OUT))
-        img = cv2.imread(str(cog_data_path(self).joinpath(IMG_OUT)), cv2.IMREAD_COLOR)
-        # apply morphology open to smooth the outline
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
-        morph = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-        # brighten dark regions
-        result = cv2.normalize(morph, None, 20, 255, cv2.NORM_MINMAX)
+        await user.avatar_url.save(self.input_image(ctx))
+        Image.open(self.input_image(ctx)).convert('RGB').resize((256, 256), Image.BICUBIC).save(self.output_image(ctx))
+        img = cv2.imread(self.output_image(ctx), cv2.IMREAD_GRAYSCALE)
+        # apply filter
+        img_blurred = cv2.bitwise_not(cv2.GaussianBlur(cv2.bitwise_not(img), (65, 65), 0))
+        img_divided = cv2.divide(img, img_blurred, scale=256)
+        img_normalized = cv2.normalize(img_divided, None, 20, 255, cv2.NORM_MINMAX)
         # save and send
-        cv2.imwrite(str(cog_data_path(self).joinpath(IMG_OUT)), result)
-        await ctx.send(file=discord.File(str(cog_data_path(self).joinpath(IMG_OUT))))
-        os.remove(cog_data_path(self).joinpath(IMG_DL))
-        os.remove(cog_data_path(self).joinpath(IMG_OUT))
+        cv2.imwrite(self.output_image(ctx), img_normalized)
+        await ctx.send(file=discord.File(self.output_image(ctx)))
+        os.remove(self.input_image(ctx))
+        os.remove(self.output_image(ctx))
+
+    @commands.command(aliases=["paintme"])
+    @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
+    async def paint(self, ctx: commands.Context, user: Union[discord.User, str] = None):
+        """Produces an oil painting of you or someone else"""
+        if user == "me" or user is None:
+            user = ctx.author
+        elif user == "you" or user == "yourself":
+            user = self.bot.user
+        elif isinstance(user, str):
+            return await ctx.send("Who?")
+        await ctx.trigger_typing()
+        # load image
+        await user.avatar_url.save(self.input_image(ctx))
+        Image.open(self.input_image(ctx)).convert('RGB').resize((256, 256), Image.BICUBIC).save(self.output_image(ctx))
+        img = cv2.imread(self.output_image(ctx), cv2.IMREAD_COLOR)
+        # apply filter
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
+        img_morphed = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+        img_normalized = cv2.normalize(img_morphed, None, 20, 255, cv2.NORM_MINMAX)
+        # save and send
+        cv2.imwrite(self.output_image(ctx), img_normalized)
+        await ctx.send(file=discord.File(self.output_image(ctx)))
+        os.remove(self.input_image(ctx))
+        os.remove(self.output_image(ctx))
+
+    def input_image(self, ctx: commands.Context) -> str:
+        return str(cog_data_path(self).joinpath(f"download_{ctx.command.name}_{ctx.author.id}.png"))
+
+    def output_image(self, ctx: commands.Context) -> str:
+        return str(cog_data_path(self).joinpath(f"output_{ctx.command.name}_{ctx.author.id}.jpg"))
