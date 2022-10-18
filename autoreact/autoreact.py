7bffc2cb7902cabb82deb0c78b416fd43c4a9ae4
Samuel <33796679+orchidalloy@users.noreply.github.com>
2022-10-18 14:50:55 -0300

    fix

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -10,12 +10,12 @@ class Autoreact(commands.Cog):
         super().__init__()
         self.bot = bot
         self.config = Config.get_conf(self, identifier=61757472)
-        self.autoreact: Dict[int, List[Tuple[str, str]]] = {}
-        self.config.register_guild(autoreact=[])
+        self.autoreacts: Dict[int, List[Tuple[str, str]]] = {}
+        self.config.register_guild(autoreacts=[])
 
     async def load_config(self):
         all_config = await self.config.all_guilds()
-        self.autoreact = {guild_id: conf['autoreact'] for guild_id, conf in all_config.items()}
+        self.autoreacts = {guild_id: conf['autoreact'] for guild_id, conf in all_config.items()}
 
     async def red_delete_data_for_user(self, requester: str, user_id: int):
         pass
@@ -26,7 +26,7 @@ class Autoreact(commands.Cog):
     async def on_message(self, message: discord.Message):
         if not message.guild or message.author.bot:
             return
-        autoreact = self.autoreact.get(message.guild.id, None)
+        autoreact = self.autoreacts.get(message.guild.id, None)
         if not autoreact:
             return
         for text, emoji in autoreact:
@@ -52,27 +52,27 @@ class Autoreact(commands.Cog):
         if len(text) > 200:
             await ctx.send("Sorry, the target text may not be longer than 200 characters.")
             return
-        async with self.config.guild(ctx.guild).autoreact() as autoreact:
+        async with self.config.guild(ctx.guild).autoreacts() as autoreacts:
             pair = (text, str(emoji))
-            if pair in autoreact:
+            if pair in autoreacts:
                 await ctx.react_quietly("✅")
                 return
-            autoreact.append(pair)
-            self.autoreact.setdefault(ctx.guild.id, [])
-            self.autoreact[ctx.guild.id].append(pair)
+            autoreacts.append(pair)
+            self.autoreacts.setdefault(ctx.guild.id, [])
+            self.autoreacts[ctx.guild.id].append(pair)
             await ctx.react_quietly("✅")
 
     @autoreact.command()
     @commands.has_permissions(manage_guild=True)
     async def removetext(self, ctx: commands.Context, *, text: str):
         """Remove existing autoreacts for a target text."""
-        self.autoreact.setdefault(ctx.guild.id, [])
-        async with self.config.guild(ctx.guild).autoreact() as autoreact:
-            to_remove = [pair for pair in autoreact if pair[0] == text]
+        self.autoreacts.setdefault(ctx.guild.id, [])
+        async with self.config.guild(ctx.guild).autoreacts() as autoreacts:
+            to_remove = [pair for pair in autoreacts if pair[0] == text]
             for pair in to_remove:
-                autoreact.remove(pair)
-                if pair in self.autoreact[ctx.guild.id]:
-                    self.autoreact[ctx.guild.id].remove(pair)
+                autoreacts.remove(pair)
+                if pair in self.autoreacts[ctx.guild.id]:
+                    self.autoreacts[ctx.guild.id].remove(pair)
             if to_remove:
                 await ctx.react_quietly("✅")
             else:
@@ -85,13 +85,13 @@ class Autoreact(commands.Cog):
         if isinstance(emoji, str) and not is_emoji(emoji):
             await ctx.send("Sorry, that doesn't seem to be a valid emoji.")
             return
-        self.autoreact.setdefault(ctx.guild.id, [])
-        async with self.config.guild(ctx.guild).autoreact() as autoreact:
-            to_remove = [pair for pair in autoreact if pair[1] == str(emoji)]
+        self.autoreacts.setdefault(ctx.guild.id, [])
+        async with self.config.guild(ctx.guild).autoreacts() as autoreacts:
+            to_remove = [pair for pair in autoreacts if pair[1] == str(emoji)]
             for pair in to_remove:
-                autoreact.remove(pair)
-                if pair in self.autoreact[ctx.guild.id]:
-                    self.autoreact[ctx.guild.id].remove(pair)
+                autoreacts.remove(pair)
+                if pair in self.autoreacts[ctx.guild.id]:
+                    self.autoreacts[ctx.guild.id].remove(pair)
             if to_remove:
                 await ctx.react_quietly("✅")
             else:
@@ -102,9 +102,9 @@ class Autoreact(commands.Cog):
         """Lists all autoreacts"""
         embed = discord.Embed(title="Server Autoreacts", color=await ctx.embed_color(), description="None")
         embed.set_footer(text=f"Page {page}")
-        if ctx.guild.id in self.autoreact and self.autoreact[ctx.guild.id]:
+        if ctx.guild.id in self.autoreacts and self.autoreacts[ctx.guild.id]:
             autoreacts = [f"{emoji} {text if '`' in text else f'`{text}`'}"
-                          for text, emoji in self.autoreact[ctx.guild.id]]
+                          for text, emoji in self.autoreacts[ctx.guild.id]]
             autoreacts = autoreacts[10*(page-1):10*page]
             if autoreacts:
                 embed.description = '\n'.join(autoreacts)
