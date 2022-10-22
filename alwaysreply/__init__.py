64e9a1d86fd52555eedf2c665bd816d4fd126f1c
Samuel <33796679+orchidalloy@users.noreply.github.com>
2022-10-22 15:39:43 -0300

    alwaysreply test

---
+++ alwaysreply/__init__.py
@@ -0,0 +1,6 @@
+from .alwaysreply import AlwaysReply
+
+__red_end_user_data_statement__ = "This cog does not store any user data."
+
+def setup(bot):
+    bot.add_cog(AlwaysReply(bot))
---
+++ alwaysreply/alwaysreply.py
@@ -0,0 +1,36 @@
+from types import MethodType
+from redbot.core import commands
+from discord.ext.commands import Context as DPYContext
+
+old_ctx_send = commands.Context.send
+
+class CustomContext(DPYContext):
+    async def send(self, content, **kwargs):
+        _filter = kwargs.pop("filter", None)
+
+        if _filter and content:
+            content = _filter(str(content))
+
+        if 'reference' not in kwargs:
+            kwargs['reference'] = self.message
+
+        return await super().send(content=content, **kwargs)
+
+
+class AlwaysReply(commands.Cog):
+    """Makes the bot always reply to commands"""
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+        commands.Context.send = MethodType(CustomContext.send, commands.Context)
+
+    def cog_unload(self):
+        commands.Context.send = old_ctx_send
+
+    async def red_delete_data_for_user(self, requester: str, user_id: int):
+        pass
+
+
+
+
