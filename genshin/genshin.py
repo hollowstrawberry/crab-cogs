58cc809fbb80138f21cc479192e96c3f32e5f546
Samuel <33796679+orchidalloy@users.noreply.github.com>
2022-11-18 01:46:36 -0300

    fix

--- genshin/genshin.py
+++ genshin/genshin.py
@@ -2,32 +2,34 @@ import discord
 from random import random, choice
 from redbot.core import commands, Config
 
-banner = {
-    "5starfeatured": ["Hu Tao"],
-    "5star": ["Keqing", "Mona", "Qiqi", "Diluc", "Jean"],
-    "5starweapon": [],
-    "4starfeatured": ["Chongyun", "Xingqiu", "Xiangling"],
-    "4star": [
-        "Xinyan", "Sucrose", "Diona", "Noelle", "Bennett", "Fischl", "Ningguang", "Beidou", "Razor", "Barbara"],
-    "4starweapon": [
-        "Rust", "Sacrificial Bow", "The Stringless", "Favonius Warbow", "Eye of Perception",
-        "Sacrificial Fragments", "The Widsith", "Favonius Codex", "Favonius Lance", "Dragon's Bane",
-        "Rainslasher", "Sacrificial Greatsword", "The Bell", "Favonius Greatsword", "Lions Roar",
-        "Sacrificial Sword", "The Flute", "Favonius Sword"],
-    "3star": [
-        "Slingshot", "Sharpshooter's Oath", "Raven Bow", "Emerald Orb", "Thrilling Tales of Dragon Slayers",
-        "Magic Guide", "Black Tassel", "Debate Club", "Bloodtainted Greatsword", "Ferrous Shadow",
-        "Skyrider Sword", "Harbinger of Dawn", "Cool Steel"]
+BANNERS = {
+    "Hu Tao": {
+        "5starfeatured": ["Hu Tao"],
+        "5star": ["Keqing", "Mona", "Qiqi", "Diluc", "Jean"],
+        "5starweapon": [],
+        "4starfeatured": ["Chongyun", "Xingqiu", "Xiangling"],
+        "4star": [
+            "Xinyan", "Sucrose", "Diona", "Noelle", "Bennett", "Fischl", "Ningguang", "Beidou", "Razor", "Barbara"],
+        "4starweapon": [
+            "Rust", "Sacrificial Bow", "The Stringless", "Favonius Warbow", "Eye of Perception",
+            "Sacrificial Fragments", "The Widsith", "Favonius Codex", "Favonius Lance", "Dragon's Bane",
+            "Rainslasher", "Sacrificial Greatsword", "The Bell", "Favonius Greatsword", "Lions Roar",
+            "Sacrificial Sword", "The Flute", "Favonius Sword"],
+        "3star": [
+            "Slingshot", "Sharpshooter's Oath", "Raven Bow", "Emerald Orb", "Thrilling Tales of Dragon Slayers",
+            "Magic Guide", "Black Tassel", "Debate Club", "Bloodtainted Greatsword", "Ferrous Shadow",
+            "Skyrider Sword", "Harbinger of Dawn", "Cool Steel"]
+    }
 }
 
-fivestars = banner["5star"] + banner["5starfeatured"] + banner["5starweapon"]
-fourstars = banner["4star"] + banner["4starfeatured"] + banner["4starweapon"]
-pull_img = {
+FIVESTARS = set(sum(banner["5star"] + banner["5starfeatured"] + banner["5starweapon"] for banner in BANNERS.values()))
+FOURSTARS = set(sum(banner["4star"] + banner["4starfeatured"] + banner["4starweapon"] for banner in BANNERS.values()))
+PULL_IMG = {
     "Hu Tao": "https://cdn.discordapp.com/attachments/541768631445618689/818653017892061194/unknown.png"
 }
-wish_img = "https://cdn.discordapp.com/attachments/541768631445618689/818649843202916362/unknown.png"
-wish_img4 = "https://media.discordapp.net/attachments/541768631445618689/879785351579832371/wish4.png"
-wish_img5 = "https://cdn.discordapp.com/attachments/541768631445618689/879785356382330901/wish5.png"
+WISH_IMG3 = "https://cdn.discordapp.com/attachments/541768631445618689/818649843202916362/unknown.png"
+WISH_IMG4 = "https://media.discordapp.net/attachments/541768631445618689/879785351579832371/wish4.png"
+WISH_IMG5 = "https://cdn.discordapp.com/attachments/541768631445618689/879785356382330901/wish5.png"
 
 
 class Genshin(commands.Cog):
@@ -39,6 +41,7 @@ class Genshin(commands.Cog):
         self.config = Config.get_conf(self, identifier=6765673686)
         default_config = {"no4star": 0, "no4starf": 0, "no5star": 0, "no5starf": 0, "inv": {}}
         self.config.register_user(**default_config)
+        self.banner = BANNERS["Hu Tao"]
 
     async def red_delete_data_for_user(self, requester: str, user_id: int):
         await self.config.user_from_id(user_id).clear()
@@ -69,7 +72,7 @@ class Genshin(commands.Cog):
             possible = self.banner["3star"]
 
         result = choice(possible)
-        if result in fourstars:
+        if result in FOURSTARS:
             userdata["no4star"] = 0
             if result in self.banner["4starfeatured"]:
                 userdata["no4starf"] = 0
@@ -80,7 +83,7 @@ class Genshin(commands.Cog):
         else:
             userdata["no4star"] += 1
             userdata["no4starf"] += 1
-            if result in fivestars:
+            if result in FIVESTARS:
                 userdata["no5star"] = 0
                 if result in self.banner["5starfeatured"]:
                     userdata["no5starf"] = 0
@@ -102,7 +105,7 @@ class Genshin(commands.Cog):
 
     @classmethod
     def formatitem(cls, item):
-        return f'{item}{" ⭐⭐⭐⭐⭐" if item in fivestars else ""}{" ⭐⭐⭐⭐" if item in fourstars else ""}'
+        return f'{item}{" ⭐⭐⭐⭐⭐" if item in FIVESTARS else ""}{" ⭐⭐⭐⭐" if item in FOURSTARS else ""}'
 
     @commands.command(aliases=["pull", "wish"])
     async def pull1(self, ctx: commands.Context, *, etc=""):
@@ -111,8 +114,8 @@ class Genshin(commands.Cog):
             return await self.pull10(ctx)
         pulled = (await self.pullx(ctx.author, 1))[0]
         embed = discord.Embed(title="Your pull", description=self.formatitem(pulled), color=await ctx.embed_color())
-        embed.set_thumbnail(url=wish_img5 if pulled in fivestars else wish_img4 if pulled in fourstars else wish_img)
-        embed.set_image(url=pull_img.get(pulled, ""))
+        embed.set_thumbnail(url=WISH_IMG5 if pulled in FIVESTARS else WISH_IMG4 if pulled in FOURSTARS else WISH_IMG3)
+        embed.set_image(url=PULL_IMG.get(pulled, ""))
         await ctx.send(embed=embed)
 
     @commands.command(aliases=["wish10"])
@@ -121,9 +124,9 @@ class Genshin(commands.Cog):
         pulled = await self.pullx(ctx.author, 10)
         pulledf = "\n".join(self.formatitem(p) for p in pulled)
         embed = discord.Embed(title="Your pulls", description=f"```md\n{pulledf}```", color=await ctx.embed_color())
-        embed.set_thumbnail(url=wish_img5 if any(p in fivestars for p in pulled) else
-                            wish_img4 if any(p in fourstars for p in pulled) else wish_img)
-        embed.set_image(url=next((pull_img.get(p) for p in pulled if p in pull_img), ""))
+        embed.set_thumbnail(url=WISH_IMG5 if any(p in FIVESTARS for p in pulled) else
+                            WISH_IMG4 if any(p in FOURSTARS for p in pulled) else WISH_IMG3)
+        embed.set_image(url=next((PULL_IMG.get(p) for p in pulled if p in PULL_IMG), ""))
 
         await ctx.send(embed=embed)
 
@@ -137,6 +140,6 @@ class Genshin(commands.Cog):
             s = "```md"
             for key, value in inv.items():
                 if key not in self.banner["3star"]:
-                    s += f'\n{value} x {key}{" ⭐⭐⭐⭐⭐" if key in fivestars else ""}'
+                    s += f'\n{value} x {key}{" ⭐⭐⭐⭐⭐" if key in FIVESTARS else ""}'
             embed = discord.Embed(title="Your inventory", description=s + "```", color=await ctx.embed_color())
             await ctx.send(embed=embed)
