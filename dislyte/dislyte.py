352c43409cbf9ee7d53ef31e14a3185c7cb743fd
Samuel <33796679+orchidalloy@users.noreply.github.com>
2022-12-11 23:44:08 -0300

    Update dislyte.py

--- dislyte/dislyte.py
+++ dislyte/dislyte.py
@@ -66,11 +66,12 @@ class Dislyte(commands.Cog):
             return
         your_speed = (your_base_speed * (1 + (your_captain_bonus / 100))) + your_bonus_speed
         enemy_speed = your_speed / your_ap * enemy_ap
-        enemy_bonus_speed = int(round(enemy_speed - (enemy_base_speed * (1 + (enemy_captain_bonus / 100)))))
+        enemy_bonus_speed = enemy_speed - (enemy_base_speed * (1 + (enemy_captain_bonus / 100)))
         embed = discord.Embed(title="🕊️ Speed Calculation", color=await ctx.embed_color())
         embed.add_field(name="Your AP", value=f"{your_ap}%", inline=True)
         embed.add_field(name="Enemy AP", value=f"{enemy_ap}%", inline=True)
-        embed.add_field(name="Enemy Bonus Speed", value=f"{enemy_bonus_speed} ± 1", inline=False)      
+        embed.add_field(name="Enemy Total Speed", value=f"{int(round(enemy_speed))} (±1)", inline=False)
+        embed.add_field(name="Enemy Speed", value=f"+{int(round(enemy_bonus_speed))} (±1)", inline=False)      
         await ctx.send(embed=embed)
 
     @commands.guild_only()
