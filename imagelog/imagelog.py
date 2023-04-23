e747e7f5b0f708280a8f71d8605dd2afaa07294d
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2023-04-23 18:51:06 -0400

    Squashed commit of the following:
    commit f88d7c57ec87773a514891ff5f2b1758b80751ea
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:49:21 2023 -0400
    
        kek
    
    commit 316e39cfe4fbb69e00f303d5e3536935b43a6c6c
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:46:10 2023 -0400
    
        fuc
    
    commit 07d07a1ca99da05dd47abeac9abd005867fec5b8
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:44:49 2023 -0400
    
        oof
    
    commit b03127dab08ced23ac9727358fec5180833ead88
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:43:41 2023 -0400
    
        oop
    
    commit 479facf98738de00f808651eacc55bd75f63b1e9
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:33:04 2023 -0400
    
        a
    
    commit 7e37505d5766b0e9b037a23f6dab80259c678546
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:32:32 2023 -0400
    
        god
    
    commit 8cdcf6e12be05f9f02976ce719d2a86caa7addf4
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:27:12 2023 -0400
    
        oof
    
    commit 470a91ce30c8fb91d1cc97e30f6713b855cb8ab2
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:24:46 2023 -0400
    
        oof
    
    commit 7b107cdc316a37b0c7ec4b67e5a1463792991b26
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:21:37 2023 -0400
    
        oop
    
    commit 1982fd766b86a1ee77cb706e047346592d4e58cd
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:16:07 2023 -0400
    
        oop
    
    commit a40a5ea133ee349f1288600b9be64b75815239c5
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:15:11 2023 -0400
    
        oop
    
    commit 6fe868f9abe93458c134e5c4d9732cf34407249f
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:14:38 2023 -0400
    
        oop
    
    commit 67aaa2804b5a48e6f3b410666984917ce1b79c41
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:13:51 2023 -0400
    
        unhide
    
    commit d389d49748806368c788b2095ee25b1523d98078
    Author: Samuel <33796679+hollowstrawberry@users.noreply.github.com>
    Date:   Sun Apr 23 18:13:37 2023 -0400
    
        voicelog and imagelog

---
+++ imagelog/imagelog.py
@@ -0,0 +1,89 @@
+import io
+import discord
+from datetime import datetime
+from redbot.core import commands, Config
+
+IMAGE_TYPES = (".png", ".jpg", ".jpeg", ".gif", ".webp")
+
+class ImageLog(commands.Cog):
+    """Logs deleted images for moderation purposes."""
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+        self.logchannels: dict[int, int] = {}
+        self.config = Config.get_conf(self, identifier=6961676567)
+        self.config.register_guild(channel=0)
+
+    async def load_config(self):
+        all_config = await self.config.all_guilds()
+        self.logchannels = {guild_id: conf['channel'] for guild_id, conf in all_config.items()}
+
+    async def red_delete_data_for_user(self, requester: str, user_id: int):
+        pass
+
+    @commands.Cog.listener(name="on_raw_message_delete")
+    async def on_raw_message_delete_listener(self, ctx: discord.RawMessageDeleteEvent):
+        message = ctx.cached_message
+        if not message or not self.logchannels.get(ctx.guild_id, 0):
+            return
+        guild = self.bot.get_guild(ctx.guild_id)
+        channel = guild.get_channel(ctx.channel_id)
+        log_channel = guild.get_channel(self.logchannels[guild.id])
+        attachments = [a for a in message.attachments if a.filename.lower().endswith(IMAGE_TYPES)]
+        if not log_channel or not attachments:
+            return
+        if await self.bot.cog_disabled_in_guild(self, guild):
+            return
+        for attachment in attachments:
+            embed = discord.Embed(
+                title="Image deleted",
+                description=message.content[:1990] if message.content else "",
+                color=message.author.color,
+                timestamp=datetime.utcnow())
+            embed.set_author(name=str(message.author), icon_url=str(message.author.avatar_url))
+            embed.add_field(name=f"Channel", value=channel.mention)
+            if channel.permissions_for(guild.me).view_audit_log:
+                deleter = None
+                async for log in guild.audit_logs(limit=2, action=discord.AuditLogAction.message_delete):
+                    if log.target.id == message.author.id and log.extra.channel.id == message.channel.id:
+                        deleter = log.user
+                        break
+                if not deleter:
+                    if channel.permissions_for(message.author).manage_messages:
+                        return # self delete by mod
+                    deleter = message.author
+                embed.add_field(name="Probably deleted by", value=deleter.mention)
+            else:
+                embed.add_field(name="Missing audit log permission", value="Oops")
+            img = io.BytesIO()
+            try:
+                await attachment.save(img, use_cached=True)
+            except:
+                file = None
+            else:
+                file = discord.File(img, filename=attachment.filename)
+            embed.set_image(url=f"attachment://{attachment.filename}")
+            await log_channel.send(embed=embed, file=file)
+
+    @commands.group(invoke_without_command=True)
+    @commands.has_permissions(manage_guild=True)
+    async def imagelog(self, ctx: commands.Context):
+        """View the current image log channel."""
+        channel_id = await self.logchannels.get(ctx.guild.id, 0)
+        if channel_id:
+            await ctx.reply(f"Deleted images are currently logged to <#{channel_id}>\nYou can change it or remove it with {ctx.prefix}imagelog setchannel")
+        else:
+            await ctx.reply(f"Image log disabled. You can assign it to the current channel with {ctx.prefix}imagelog setchannel")
+
+    @imagelog.command(invoke_without_command=True)
+    async def setchannel(self, ctx: commands.Context):
+        """Sets the image log channel to the current channel."""
+        if ctx.channel.id == self.logchannels.get(ctx.guild.id, 0):
+            self.logchannels[ctx.guild.id] = 0
+            await self.config.guild(ctx.guild).channel.set(0)
+            await ctx.reply("Removed image log channel.")
+        else:
+            self.logchannels[ctx.guild.id] = ctx.channel.id
+            await self.config.guild(ctx.guild).channel.set(ctx.channel.id)
+            await ctx.reply(f"Set image log channel to {ctx.channel.mention}. Use this command again to remove it.")
---
+++ voicelog/voicelog.py
@@ -0,0 +1,61 @@
+import discord
+from datetime import datetime
+from redbot.core import commands, Config
+
+class VoiceLog(commands.Cog):
+    """Logs users joining and leaving a VC, inside the VC chat itself."""
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+        self.allowedguilds = set()
+        self.config = Config.get_conf(self, identifier=7669636567)
+        self.config.register_guild(enabled=False)
+
+    async def load_config(self):
+        all_config = await self.config.all_guilds()
+        self.logchannels = set(guild_id for guild_id, conf in all_config.items() if conf['enabled'])
+
+    async def red_delete_data_for_user(self, requester: str, user_id: int):
+        pass
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        guild = member.guild
+        if guild.id not in self.allowedguilds:
+            return
+        if before.channel == after.channel:
+            return
+        if await self.bot.cog_disabled_in_guild(self, guild):
+            return
+        embed = discord.Embed(color=member.color, timestamp=datetime.utcnow())
+        if not before.channel:
+            embed.set_author(name="Connected", icon_url=str(member.avatar_url))
+            embed.description = f"{member.mention} has joined {after.channel.mention}"
+        elif not after.channel:
+            embed.set_author(name="Disconnected", icon_url=str(member.avatar_url))
+            embed.description = f"{member.mention} has left {before.channel.mention}"
+        else:
+            embed.set_author(name="Moved", icon_url=str(member.avatar_url))
+            embed.description = f"{member.mention} has moved from {before.channel.mention} to {after.channel.mention}"
+        await (after.channel or before.channel).send(embed=embed)
+
+    @commands.group(invoke_without_command=True)
+    @commands.has_permissions(manage_guild=True)
+    async def voicelog(self, ctx: commands.Context):
+        """Voice Log configuration"""
+        await ctx.send_help()
+
+    @voicelog.command()
+    async def enable(self, ctx: commands.Context):
+        """Enable voice log for the whole guild."""
+        self.allowedguilds.add(ctx.guild.id)
+        await self.config.guild(ctx.guild).enabled.set(True)
+        await ctx.react_quietly('✅')
+
+    @voicelog.command()
+    async def disable(self, ctx: commands.Context):
+        """Disable voice log for the whole guild."""
+        self.allowedguilds.remove(ctx.guild.id)
+        await self.config.guild(ctx.guild).enabled.set(False)
+        await ctx.react_quietly('✅')
