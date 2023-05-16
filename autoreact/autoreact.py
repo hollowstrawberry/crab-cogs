eb0be5a2dbcc3a4a64e7cb97ec37d9b21b418c78
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2023-05-16 17:04:31 -0400

    Fix autoreact list (#19)

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -91,7 +91,7 @@ class Autoreact(commands.Cog):
     @commands.has_permissions(manage_guild=True)
     @commands.bot_has_permissions(add_reactions=True)
     async def add(self, ctx: commands.Context, emoji: Union[discord.Emoji, str], *, pattern: str):
-        """Add a new autoreact using regex."""
+        """Add a new autoreact using regex. Tip: (?i) in a regex makes it case-insensitive."""
         if isinstance(emoji, str) and not is_emoji(emoji):
             await ctx.send("Sorry, that doesn't seem to be a valid emoji to react with.")
             return
@@ -140,13 +140,14 @@ class Autoreact(commands.Cog):
         autoreacts = [f"{emoji} {regex.pattern if '`' in regex.pattern else f'`{regex.pattern}`'}"
                       for emoji, regex in self.autoreacts[ctx.guild.id].items()]
         pages = []
-        for i, batch in enumerate(batched(pages, 10)):
+        for i, batch in enumerate(batched(autoreacts, 10)):
             embed = discord.Embed(title="Server Autoreacts", color=await ctx.embed_color())
-            embed.set_footer(text=f"Page {i+1}/{(9+len(autoreacts))//10}")
-            embed.description = '\n'.join(autoreacts)
+            if len(autoreacts) > 10:
+                embed.set_footer(text=f"Page {i+1}/{(9+len(autoreacts))//10}")
+            embed.description = '\n'.join(batch)
             pages.append(embed)
         if len(pages) == 1:
-            await ctx.send(embed=embed)
+            await ctx.send(embed=pages[0])
         else:
             await SimpleMenu(pages, timeout=600).start(ctx)
         
