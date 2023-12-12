596d95f68398a77c8b418a82c87ac658808731fa
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2023-12-12 18:29:25 -0300

    gelbooru cog

--- gelbooru/__init__.py
+++ gelbooru/__init__.py
@@ -1,4 +1,4 @@
-from .boorucog import Booru
+from .gelbooru import Booru
 from redbot.core.utils import get_end_user_data_statement
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
