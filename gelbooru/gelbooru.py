3d452b91888899fe8a0e6c5e77dec33b54714cd7
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2023-12-13 22:58:23 -0300

    Fix missing booru results by making http requests manually

--- gelbooru/gelbooru.py
+++ gelbooru/gelbooru.py
@@ -1,7 +1,7 @@
 import discord
-import booru
-import json
+import aiohttp
 import re
+import random
 import logging
 from redbot.core import commands, app_commands, Config
 
@@ -10,7 +10,10 @@ log = logging.getLogger("red.crab-cogs.boorucog")
 EMBED_COLOR = 0xD7598B
 EMBED_ICON = "https://i.imgur.com/FeRu6Pw.png"
 IMAGE_TYPES = (".png", ".jpeg", ".jpg", ".webp", ".gif")
-TAG_BLACKLIST = "loli guro video"
+TAG_BLACKLIST = ["loli", "guro", "video"]
+HEADERS = {
+    "User-Agent": f"crab-cogs/v1 (https://github.com/hollowstrawberry/crab-cogs);"
+}
 
 class Booru(commands.Cog):
     """Searches images on Gelbooru with slash command and tag completion support."""
@@ -18,22 +21,23 @@ class Booru(commands.Cog):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
-        self.gel = None
         self.tag_cache = {}
         self.config = Config.get_conf(self, identifier=62667275)
         self.config.register_global(tag_cache={})
 
     async def cog_load(self):
         self.tag_cache = await self.config.tag_cache()
-        keys = await self.bot.get_shared_api_tokens("gelbooru")
-        api_key, user_id = keys.get("api_key"), keys.get("user_id")
-        if api_key and user_id:
-            self.gel = booru.Gelbooru(api_key, user_id)
-        else:
-            self.gel = booru.Gelbooru()
 
     async def red_delete_data_for_user(self, requester: str, user_id: int):
         pass
+
+    @commands.command()
+    @commands.is_owner()
+    async def boorudeletecache(self, ctx: commands.Context):
+        self.tag_cache = {}
+        async with self.config.tag_cache() as tag_cache:
+            tag_cache.clear()
+        await ctx.react_quietly("✅")
 
     @commands.hybrid_command(aliases=["gelbooru"])
     @app_commands.describe(tags="Will suggest tags with autocomplete. Separate tags with spaces.")
@@ -49,29 +53,25 @@ class Booru(commands.Cog):
         tags = tags.strip()
         if tags.lower() in ["none", "error"]:
             tags = ""
-
         if not ctx.channel.nsfw:
             tags = re.sub(" ?rating:[^ ]+", "", tags)
             tags += " rating:general"
 
-        result = {}
-        while not result.get("file_url", "").endswith(IMAGE_TYPES):
-            try:
-                response = await self.gel.search(query=tags, block=TAG_BLACKLIST, gacha=True)
-                result = json.loads(response)
-            except KeyError:
-                description = "💨 No results..."
-                if not ctx.channel.nsfw:
-                    description += " (safe mode)"
-                await ctx.send(embed=discord.Embed(description=description, color=EMBED_COLOR))
-                return
-            except Exception as e:
-                log.error("Failed to grab image from Gelbooru", exc_info=e)
-                await ctx.send("Sorry, there was an error trying to grab an image from Gelbooru. Please try again or contact the bot owner.")
-                return
+        try:
+            result = await self.grab_image(tags)
+        except Exception as e:
+            log.error("Failed to grab image from Gelbooru", exc_info=e)
+            await ctx.send("Sorry, there was an error trying to grab an image from Gelbooru. Please try again or contact the bot owner.")
+            return
+        if not result:
+            description = "💨 No results..."
+            if not ctx.channel.nsfw:
+                description += " (safe mode)"
+            await ctx.send(embed=discord.Embed(description=description, color=EMBED_COLOR))
+            return
 
         embed = discord.Embed(color=EMBED_COLOR)
-        embed.set_author(name="Booru Post", url=result.get("post_url", None), icon_url=EMBED_ICON)
+        embed.set_author(name="Booru Post", url=f"https://gelbooru.com/index.php?page=post&s=view&id={result['id']}", icon_url=EMBED_ICON)
         embed.set_image(url=result["file_url"] if result["width"]*result["height"] < 4200000 else result["sample_url"])
         if result.get("source", ""):
             embed.description = f"[🔗 Original Source]({result['source']})"
@@ -134,11 +134,40 @@ class Booru(commands.Cog):
     async def get_tags(self, query):
         if query in self.tag_cache:
             return self.tag_cache[query].split(' ')
-        response = await self.gel.find_tags(query=f"*{query}*")
-        results = json.loads(response)
-        results = [res for res in results if '%' not in res]
-        results = results[:20]
+        results = await self.grab_tags(query)
         self.tag_cache[query] = ' '.join(results)
         async with self.config.tag_cache() as tag_cache:
             tag_cache[query] = self.tag_cache[query]
         return results
+
+    async def grab_tags(self, query):
+        url = f"https://gelbooru.com/index.php?page=dapi&s=tag&q=index&json=1&sort=desc&order_by=index_count&name_pattern=%25{query}%25"
+        api = await self.bot.get_shared_api_tokens("gelbooru")
+        api_key, user_id = api.get("api_key"), api.get("user_id")
+        if api_key and user_id:
+            url += f"&api_key={api_key}&user_id={user_id}"
+        async with aiohttp.ClientSession(headers=HEADERS) as session:
+            async with session.get(url) as resp:
+                data = await resp.json()
+                if not data or "tag" not in data:
+                    return []
+                return [tag["name"] for tag in data["tag"]][:20]
+
+    async def grab_image(self, query):
+        query = query.lower().replace('%', "")
+        tags = [tag for tag in query.split(' ') if tag]
+        tags = [tag for tag in tags if tag not in TAG_BLACKLIST]
+        tags += [f"-{tag}" for tag in TAG_BLACKLIST]
+        query = ' '.join(tags)
+        url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=" + query.replace(' ', '+')
+        api = await self.bot.get_shared_api_tokens("gelbooru")
+        api_key, user_id = api.get("api_key"), api.get("user_id")
+        if api_key and user_id:
+            url += f"&api_key={api_key}&user_id={user_id}"
+        async with aiohttp.ClientSession(headers=HEADERS) as session:
+            async with session.get(url) as resp:
+                data = await resp.json()
+                if not data or "post" not in data:
+                    return None
+                images = [img for img in data["post"] if img["file_url"].endswith(IMAGE_TYPES)]
+                return random.choice(images)
