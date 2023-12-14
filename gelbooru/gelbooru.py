b884e683384d06496d86ddc97712b93bc7334d4b
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2023-12-13 21:11:21 -0300

    Update gelbooru.py

--- gelbooru/gelbooru.py
+++ gelbooru/gelbooru.py
@@ -102,6 +102,8 @@ class Booru(commands.Cog):
             results = ["rating:general"]
             if interaction.channel.nsfw:
                 results += ["rating:sensitive", "rating:questionable", "rating:explicit"]
+            else:
+                excluded = False
         elif "score" in last.lower():
             excluded = False
             results = ["score:>10", "score:>100"]
