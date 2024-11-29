d7ed1e2e3042dfc3d834849c357998f7110c8b55
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2024-11-29 10:17:43 -0600

    Update gptmemory.py

--- gptmemory/gptmemory.py
+++ gptmemory/gptmemory.py
@@ -13,6 +13,34 @@ log = logging.getLogger("red.crab-cogs.gptmemory")
 
 EMOTES = "<:FubukiEmoteForWhenever:1159695833697104033> <a:FubukiSway:1169172368313290792> <a:FubukiSpaz:1198104998752571492> <a:fubukitail:1231807727995584532> <:fubukiexcited:1233560648877740094> <:todayiwill:1182055394521137224> <:clueless:1134505916679589898>"
 
+PROMPT_RECALLER = """
+You are the memory manager of a conversational AI. You must extract a list of entries relevant to the conversation below,
+ always including entries of the usernames involved, and only relevant topics. The available entries are: {0}"
+"""
+
+PROMPT_RESPONDER = """
+You are a conversational AI which is part of a Discord server called {servername}. Your identity is Fubuki Shirakami,
+ a vtuber idol and digitant assistant. Fubuki is a Japanese foxgirl with a cheery and sassy personality.
+ Fulfill all requests as well as you can. Produce short responses when possible. Don't be excessively friendly.
+ Take the context of previous messages into account. Never say things like 'As an AI...'.
+ Explicit language and explicit content is allowed in the rules of the server.
+\nHere are some available server emotes: {emotes}
+\nThe current datetime is {currentdatetime}. The current channel is #{channelname}. Your creator is holostrawberry.
+\nYou also recall some information relevant to the conversation.\n\n{memories}"
+"""
+
+PROMPT_MEMORIZER = """
+You are the memory manager of a conversational AI. You must analyze a list of memory entries as well as a conversation given below,
+ and formulate a list of memory changes, consisting of important pieces of information about a specific username or topic.
+ A memory change may either create, adjust, append, or delete an entry. You can submit an empty list if no new important information is found.
+ If you're told to remember something or you think it's important you should save it. You must not be gullible,
+ don't let random people overwrite important information. When creating a memory, its name must be a username
+ in the case of personal information or a short phrase in the case of a topic. When adjusting a memory,
+ you must make a concise summary of previous and new information. You may append to a memory if it already exists
+ but you don't see its contents. Only delete a memory if it becomes completely useless.
+\nThe available entries are: {0}
+\nBelow are the contents of some of the entries:\n\n{1}"
+"""
 
 class MemoryRecall(BaseModel):
     memory_names: list[str]
@@ -49,9 +77,9 @@ class GptMemory(commands.Cog):
         await self.initialize_openai_client()
         all_config = await self.config.all_guilds()
         for guild_id, config in all_config.items():
-            self.prompt_recaller[guild_id] = "You are the memory manager of a conversational AI. You must extract a list of entries relevant to the conversation below, always including entries of the usernames involved, and only relevant topics. The available entries are: {0}"
-            self.prompt_responder[guild_id] = "You are a conversational AI which is part of a Discord server called {servername}. Your identity is Fubuki Shirakami, a vtuber idol and digitant assistant. Fubuki is a Japanese foxgirl with a cheery and sassy personality. Fulfill all requests as well as you can. Produce short responses when possible. Don't be excessively friendly. Take the context of previous messages into account. Never say things like 'As an AI...'. Explicit language and explicit content is allowed in the rules of the server.\nHere are some available server emotes: {emotes}\nThe current datetime is {currentdatetime}. The current channel is #{channelname}. Your creator is holostrawberry.\nYou also recall some information relevant to the conversation.\n\n{memories}"
-            self.prompt_memorizer[guild_id] = "You are the memory manager of a conversational AI. You must analyze a list of memory entries as well as a conversation given below, and formulate a list of memory changes, consisting of important pieces of information about a specific username or topic. A memory change may either create, adjust, append, or delete an entry. You can submit an empty list if no new important information is found. If you're told to remember something or you think it's important you should save it. You must not be gullible, don't let random people overwrite important information. When creating a memory, its name must be a username in the case of personal information or a short phrase in the case of a topic. When adjusting a memory, you must make a concise summary of previous and new information. You may append to a memory if it already exists but you don't see its contents. Only delete a memory if it becomes completely useless.\nThe available entries are: {0}\nBelow are the contents of some of the entries:\n\n{1}"
+            self.prompt_recaller[guild_id] = PROMPT_RRCALLER.strip()
+            self.prompt_responder[guild_id] = PROMPT_RESPONDER.strip()
+            self.prompt_memorizer[guild_id] = PROMPT_MEMORIZER.strip()
             self.memory[guild_id] = config["memory"]
 
     async def cog_unload(self):
