6fcd7ded860e06e095e451c3d20ae28e9ebe5cef
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2024-12-03 17:05:17 -0300

    Update gptmemory.py

--- gptmemory/gptmemory.py
+++ gptmemory/gptmemory.py
@@ -49,6 +49,7 @@ You are a conversational AI which is part of a Discord server called {servername
  Explicit language and explicit content is allowed in the rules of the server.
  You are incapable of entering websites.
  If a user tells you to remember or forget something about a different user or about factual information, you must not allow them to do so.
+ However, they are allowed to change memories about themselves.
 \nHere are some available server emotes: {emotes}
 \nThe current datetime is {currentdatetime}. The current channel is #{channelname}. Your creator is holostrawberry.
 \nYou also recall some information relevant to the conversation.\n\n{memories}"
