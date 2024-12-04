e2c931aea0327897758c6942cb9e8394dbcc62f3
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2024-12-04 18:23:26 -0300

    Update function_calling.py

--- gptmemory/function_calling.py
+++ gptmemory/function_calling.py
@@ -129,7 +129,7 @@ class WolframAlphaFunctionCall(FunctionBase):
     schema = ToolCall(
         Function(
             name="ask_wolframalpha",
-            description="Asks Wolfram Alpha about math, exchange rates, or the weather, but not for unit conversions.",
+            description="Asks Wolfram Alpha about math, exchange rates, or the weather.",
             parameters=Parameters(
                     properties={
                         "query": {
