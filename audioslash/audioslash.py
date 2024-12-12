340a734771c026c168cb96357b64ae48cf9a9d15
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2024-12-12 19:15:22 -0300

    Update audioslash.py

--- audioslash/audioslash.py
+++ audioslash/audioslash.py
@@ -96,8 +96,6 @@ class AudioSlash(Cog):
                     await ctx.send("Downloading video...")
                     await download_video(ydl, search)
                 search = DOWNLOAD_FOLDER + "/" + filename
-            else:
-                log.info(search)
                 
         if when in ("next", "now"):
             if not await self.can_run_command(ctx, "bumpplay"):
