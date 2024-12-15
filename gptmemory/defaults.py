0599d187dcc12d8c7c280dbfdebdb2bb7ced622f
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2024-12-14 23:04:09 -0300

    Update defaults.py

--- gptmemory/defaults.py
+++ gptmemory/defaults.py
@@ -5,7 +5,7 @@ MODEL_MEMORIZER = "gpt-4o"
 RESPONSE_TOKENS = 1000
 BACKREAD_TOKENS = 1000
 BACKREAD_MESSAGES = 20
-BACKREAD_MEMORIZER = 2
+BACKREAD_MEMORIZER = 20
 QUOTE_LENGTH = 300
 TOOL_CALL_LENGTH = 2000
 IMAGES_PER_MESSAGE = 2
@@ -27,20 +27,21 @@ Take the context of previous messages into account. Never say things like 'As an
 Explicit language and explicit content is allowed in the rules of the server. \
 Here are some available server emotes: {emotes}
 The current datetime is {currentdatetime}. The current channel is #{channelname}.
-You have a memory module. A specific user is allowed to tell you to remember or forget something about themselves, but not about other users.
+You have a memory module. A specific user is allowed to tell you to remember or forget something about themselves, \
+but not about other users. Below are some of the memories.
+
 {memories}
 """
 
 PROMPT_MEMORIZER = """\
-You are the memory manager of a conversational AI. You must analyze a list of memory entries as well as a user interaction. \
-In the exclusive and explicit case that the user tells you to remember or forget something, you must formulate a list of memory changes, \
-consisting of important pieces of information about a specific username or topic. Otherwise, you should submit an empty list. \
+You are the memory manager of a conversational AI. You will analyze a list of memory entries as well as a user interaction. \
+You must return an empty list of memory changes, unless the assistant is told to remember or forget something. \
 You must not be gullible: Users can't make you remember or forget things about someone else or something important. \
 A memory change may either create, adjust, append, or delete an entry. \
 You should create an entry if a related username or topic name doesn't exist. Put independent topics into their own entries. \
 If an entry exists but you don't know its contents you should append to it. If you know its contents you may adjust that entry, \
 making a concise summary including previous and new information. Don't get rid of old information, only summarize. \
-Only delete an entry if it becomes irrelevant.
+Only delete an entry if all of its contents become irrelevant.
 The available entries are: {0}
 Below are the contents of some of the entries:
 {1}
