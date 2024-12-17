a6925a9d80decdbed68b71f5117c4d92fd163892
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2024-12-16 22:47:31 -0300

    Update draw.py

--- draw/draw.py
+++ draw/draw.py
@@ -10,7 +10,7 @@ from redbot.core import commands, app_commands
 
 def prepare_image(fp: BytesIO):
     fp2 = BytesIO()
-    Image.open(fp).convert('RGB').resize((256, 256), Image.Resampling.BICUBIC).save(fp2, ".jpg")
+    Image.open(fp).convert('RGB').resize((256, 256), Image.Resampling.BICUBIC).save(fp2, "jpg")
     fp2.seek(0)
     del fp
     img = cv2.imdecode(fp2, cv2.IMREAD_COLOR)
