d6a73dff0edc916cf74b1631cdf9141f0ac72ae9
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-01-04 23:50:16 -0300

    Update constants.py

--- imagescanner/constants.py
+++ imagescanner/constants.py
@@ -13,7 +13,7 @@ PARAM_GROUP_REGEX = re.compile(rf", [^:]+: {{.+?{LOOKAHEAD_PATTERN}}}")
 HASHES_GROUP_REGEX = re.compile(rf", Hashes: ({{.+?{LOOKAHEAD_PATTERN}}})")
 
 PARAMS_BLACKLIST = [
-    "Template", "hashes",
+    "Template", "hashes", "Version",
     "ADetailer confidence", "ADetailer mask", "ADetailer dilate", "ADetailer denoising",
     "ADetailer inpaint", "ADetailer version", "ADetailer prompt", "ADetailer use", "ADetailer checkpoint",
     "RP Divide", "RP Ma", "RP Prompt", "RP Calc", "RP Ratio", "RP Base", "RP Use", "RP LoRA", "RP Options", "RP Flip", "RP threshold",
