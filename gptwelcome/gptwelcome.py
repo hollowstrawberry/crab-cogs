b81b9fc25964d5d1e43e27a28706f2148697e592
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-05-18 00:41:51 -0400

    gptwelcome

---
+++ gptwelcome/gptwelcome.py
@@ -0,0 +1,117 @@
+import discord
+from typing import Literal, Optional, Dict
+from redbot.core import commands, Config
+from redbot.core.bot import Red
+
+from openai import AsyncOpenAI
+from tiktoken import encoding_for_model
+
+MODEL = "gpt-4o"
+
+
+class GptWelcome(commands.Cog):
+    def __init__(self, bot: Red):
+        super().__init__()
+        self.bot = bot
+        self.openai_client: Optional[AsyncOpenAI] = None
+        self.config = Config.get_conf(self, identifier=1947582011)
+        self.config.register_guild(**{
+            "enabled": False,
+            "prompt": "You are in a Discord server and are tasked with welcoming new users. When welcoming a user, give them a personalized message according to their username, and direct them to the rules channnel.",
+        })
+
+    async def cog_load(self):
+        await self.initialize_openai_client()
+
+    async def cog_unload(self):
+        if self.openai_client:
+            await self.openai_client.close()
+
+    async def initialize_openai_client(self):
+        api_key = (await self.bot.get_shared_api_tokens("openai")).get("api_key")
+        if not api_key:
+            return
+        self.openai_client = AsyncOpenAI(api_key=api_key)
+
+    @commands.Cog.listener()
+    async def on_red_api_tokens_update(self, service_name, _):
+        await self.initialize_function_calls()
+        if service_name == "openai":
+            await self.initialize_openai_client()
+
+    @commands.Cog.listener()
+    async def on_message_without_command(self, message: discord.Message):
+        if message.type != discord.MessageType.new_member:
+            return
+        
+        ctx: commands.Context = await self.bot.get_context(message)  # noqa
+        if await self.bot.cog_disabled_in_guild(self, ctx.guild):
+            return
+        if not await self.bot.ignored_channel_or_guild(ctx):
+            return
+        if not await self.bot.allowed_by_whitelist_blacklist(ctx.author):
+            return
+        
+        await self.welcome_user(ctx)
+
+    async def welcome_user(self, ctx: commands.Context):
+        if not self.openai_client:
+            await self.initialize_openai_client()
+        if not self.openai_client:
+            return
+        
+        prompt = await self.config.guild(ctx.guild).prompt()
+        if not prompt:
+            return
+        
+        messages = [
+            { "role": "system", "content": prompt },
+            { "role": "user", "content": f"The following user has joined the server: {ctx.author.name}" },
+        ]
+        response = await self.openai_client.beta.chat.completions.parse(
+            model=MODEL,
+            messages=messages
+        )
+        completion = response.choices[0].message
+        
+        await ctx.reply(content=completion, mention_author=True)
+
+
+    @commands.group(name="gptwelcome", aliases=["aiwelcome", "llmwelcome"])
+    @commands.is_owner()
+    async def gptwelcome(self, _: commands.Context):
+        """Base command for configuring the GPT Welcome cog."""
+        pass
+
+    @gptwelcome.command("enable")
+    async def gptwelcome_enable(self, ctx: commands.Context):
+        """Enables GPT Welcome for this server."""
+        await self.config.guild(ctx.guild).enabled.set(True)
+        await ctx.tick("Enabled")
+
+    @gptwelcome.command("disable")
+    async def gptwelcome_enable(self, ctx: commands.Context):
+        """Disable GPT Welcome for this server."""
+        await self.config.guild(ctx.guild).enabled.set(False)
+        await ctx.tick("Disabled")
+
+    @gptwelcome.command(name="prompt")
+    async def gptwelcome_prompt(self, ctx: commands.Context, *, prompt: str):
+        """Gives you the current prompt or sets a new prompt for the GPT welcomer."""
+        if not prompt or not prompt.strip():
+            prompt = await self.config.guild(ctx.guild).prompt()
+            await ctx.reply(f"`current welcomer prompt`\n>>> {prompt or '*None*'}", mention_author=False)
+        else:
+            await self.config.guild(ctx.guild).prompt.set(prompt.strip())
+            await ctx.reply(f"`new welcomer prompt`\n>>> {prompt.strip()}", mention_author=False)
+
+    @gptwelcome.command(name="test")
+    async def gptwelcome_test(self, ctx: commands.Context):
+        """Simulates you joining the server"""
+        if not await self.config.guild(ctx.guild).enabled():
+            await ctx.reply("GPT welcomer not enabled.")
+            return
+        if not await self.config.guild(ctx.guild).prompt():
+            await ctx.reply("Prompt not set.")
+            return
+        await self.welcome_user(ctx)
