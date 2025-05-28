f4df6630394b2ac5eff2acc0b00964003b30bb1a
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-05-28 19:00:05 -0400

    Update playerview.py

--- audioplayer/playerview.py
+++ audioplayer/playerview.py
@@ -19,7 +19,7 @@ class PlayerView(View):
     @discord.ui.button(emoji="🔽", style=discord.ButtonStyle.grey)
     async def queue(self, inter: discord.Interaction, _):
         audio: Optional[Audio] = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, audio, "queue")
+        ctx = await self.get_context(inter, audio, "queue", ephemeral=True)
         if not await self.can_run_command(ctx, "queue"):
             await inter.response.send_message("You're not allowed to perform this action.")
             return
@@ -32,7 +32,7 @@ class PlayerView(View):
     @discord.ui.button(emoji="⏪", style=discord.ButtonStyle.grey)
     async def previous(self, inter: discord.Interaction, _):
         audio: Optional[Audio] = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, audio, "prev")
+        ctx = await self.get_context(inter, audio, "prev", ephemeral=False)
         if not await self.can_run_command(ctx, "prev"):
             await inter.response.send_message("You're not allowed to perform this action.")
             return
@@ -47,7 +47,7 @@ class PlayerView(View):
     @discord.ui.button(emoji="⏸️", style=discord.ButtonStyle.grey)
     async def pause(self, inter: discord.Interaction, _):
         audio: Optional[Audio] = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, audio, "pause")
+        ctx = await self.get_context(inter, audio, "pause", ephemeral=True)
         if not await self.can_run_command(ctx, "pause"):
             await inter.response.send_message("You're not allowed to perform this action.")
             return
@@ -62,7 +62,7 @@ class PlayerView(View):
     @discord.ui.button(emoji="⏩", style=discord.ButtonStyle.grey)
     async def skip(self, inter: discord.Interaction, _):
         audio: Optional[Audio] = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, audio, "skip")
+        ctx = await self.get_context(inter, audio, "skip", ephemeral=False)
         if not await self.can_run_command(ctx, "skip"):
             await inter.response.send_message("You're not allowed to perform this action.")
             return
@@ -77,7 +77,7 @@ class PlayerView(View):
     @discord.ui.button(emoji="⏹️", style=discord.ButtonStyle.grey)
     async def stop(self, inter: discord.Interaction, _):
         audio: Optional[Audio] = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, audio, "stop")
+        ctx = await self.get_context(inter, audio, "stop", ephemeral=False)
         if not await self.can_run_command(ctx, "stop"):
             await inter.response.send_message("You're not allowed to perform this action.")
             return
@@ -89,7 +89,7 @@ class PlayerView(View):
         else:
             await self.cog.update_player(ctx.guild, ctx.channel, audio)
 
-    async def get_context(self, inter: discord.Interaction, cog: Audio, command_name: str) -> commands.Context:
+    async def get_context(self, inter: discord.Interaction, cog: Audio, command_name: str, ephemeral: bool) -> commands.Context:
         prefix = await self.cog.bot.get_prefix(self.message)
         prefix = prefix[0] if isinstance(prefix, list) else prefix
         fake_message = copy(self.message)
@@ -97,7 +97,7 @@ class PlayerView(View):
         fake_message.author = inter.user
         ctx: commands.Context = await self.cog.bot.get_context(fake_message)  # noqa
         async def send(self, *args, **kwargs):
-            await inter.response.send_message(content=kwargs.get("content"), embed=kwargs.get("embed"), ephemeral=True)
+            await inter.response.send_message(content=kwargs.get("content"), embed=kwargs.get("embed"), ephemeral=ephemeral)
         ctx.send = types.MethodType(send, ctx)  # prevent pause/skip buttons from sending a message
         return ctx
 
