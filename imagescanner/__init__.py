bf17439d76191dc095a2363114f6f915724070b4
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-08-24 17:45:50 -0400

    SD_Prompt_Reader.ImageDataReader logger set to error

--- imagescanner/__init__.py
+++ imagescanner/__init__.py
@@ -1,6 +1,9 @@
 from .imagescanner import ImageScanner
 from redbot.core.utils import get_end_user_data_statement
 
+import logging
+logging.getLogger("SD_Prompt_Reader.ImageDataReader").setLevel(logging.ERROR)
+
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 async def setup(bot):
