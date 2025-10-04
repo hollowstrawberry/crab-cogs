e85fa2d76606a7e8e8adfb7363b1f1a222da0135
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-10-04 19:04:55 -0300

    rematch timeout

--- minigames/views/rematch_view.py
+++ minigames/views/rematch_view.py
@@ -7,7 +7,7 @@ from minigames.base import Minigame
 
 class RematchView(discord.ui.View):
     def __init__(self, game: Minigame):
-        super().__init__(timeout=None)
+        super().__init__()
         self.game = game
         self.message: Optional[discord.Message] = None
         if not self.game.is_cancelled():
@@ -19,14 +19,17 @@ class RematchView(discord.ui.View):
         assert interaction.message and isinstance(interaction.user, discord.Member)
         if interaction.user not in self.game.players:
             return await interaction.response.send_message("You didn't play this game!", ephemeral=True)
+        
         temp_players = list(self.game.players)
         temp_players.remove(interaction.user)
         opponent = temp_players[0]
         players = [interaction.user, opponent] if opponent.bot else [opponent, interaction.user]
+
+        self.stop()
         await self.game.cog.base_minigame_cmd(type(self.game), interaction, players, opponent.bot)
+        await self.on_timeout()
         
     async def on_timeout(self):
-        await super().on_timeout()
         if self.message:
             try:
                 await self.message.edit(view=None)
