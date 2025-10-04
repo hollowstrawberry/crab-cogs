35aa22c5b42960ea9d0c01a150a02de084e5861b
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-10-04 18:26:10 -0300

    Update minigames.py

--- minigames/minigames.py
+++ minigames/minigames.py
@@ -59,7 +59,7 @@ class Minigames(commands.Cog):
                                 ):
         assert ctx.guild and isinstance(ctx.author, discord.Member) and isinstance(ctx.channel, discord.TextChannel)
 
-        log.info(game_cmd.__name__ if game_cmd else None)
+        log.info(game_cmd)
         
         # Game already exists
         if ctx.channel.id in self.games and not self.games[ctx.channel.id].is_finished():
