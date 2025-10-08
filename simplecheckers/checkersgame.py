5dee20c9c89ec6922c826c92b9dd50ab43106d4f
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-10-07 23:52:16 -0300

    fuc

--- simplecheckers/checkersgame.py
+++ simplecheckers/checkersgame.py
@@ -62,17 +62,7 @@ class CheckersGame(BaseCheckersGame):
             await self.cog.config.channel(self.channel).players.set([player.id for player in self.players])
 
     async def move_user(self, move_str: str) -> Tuple[bool, str]:
-        try:
-            steps = [int(s) for s in move_str.split("-" if "-" in move_str else "x")]
-            if any(s < 1 or s > 32 for s in steps):
-                raise ValueError
-        except ValueError:
-            return False, "That move is invalid. Your move must be a sequence of numbers representing the cells 1 to 32."
-        legal_moves = self.board.legal_moves()
-        log.info([m.hub_move for m in legal_moves])
         move = draughts.Move(self.board, hub_move=move_str)
-        if move not in legal_moves:
-            return False, "That move is illegal in the current state of the board, try something else."
         await self.do_move(move)
         return True, ""
             
