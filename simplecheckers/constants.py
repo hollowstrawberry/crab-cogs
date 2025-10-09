78ce870faacf7bed9e77df6b2545b326c5982f4a
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2025-10-08 22:49:35 -0300

    Typo

--- simplecheckers/constants.py
+++ simplecheckers/constants.py
@@ -1,7 +1,7 @@
 
 ENGLISH_CHECKERS_INSTRUCTIONS = """\
 This variant of checkers, often known as English draughts or American checkers, has the following rules:
-1. Players take turns moving their pieces diagonally forward."
+1. Players take turns moving their pieces diagonally forward.
 2. If you can capture an opponent's piece by jumping over it, you have to do it.
 3. You may also jump over multiple pieces in sequence in a single move.
 4. When a piece reaches the far side, it becomes a “king” and can now move both backwards and forwards.
