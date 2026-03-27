a0781654a67e75e7a5dc2c21efc7efd9b018dbd2
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-03-27 19:34:43 -0300

    typo

--- imagescanner/constants.py
+++ imagescanner/constants.py
@@ -12,9 +12,9 @@ PARAM_REGEX = re.compile(rf" ?([^:]+): (.+?),{LOOKAHEAD_PATTERN}")
 PARAM_GROUP_REGEX = re.compile(rf", [^:]+: {{.+?{LOOKAHEAD_PATTERN}}}")
 
 PARAMS_BLACKLIST = [
-    "Template", "Version", "Hires prompt", "Hires negative"
+    "Template", "Version", "Hires prompt", "Hires negative",
     "ADetailer mask", "ADetailer dilate", "ADetailer prompt", "ADetailer use", "ADetailer checkpoint", "ADetailer sampler", "ADetailer scheduler",
-    "ADetailer inpaint", "ADetailer min", "ADetailer method", "ADetailer hires", "ADetailer mask"
+    "ADetailer inpaint", "ADetailer min", "ADetailer method", "ADetailer hires",
     "RP Divide", "RP Ma", "RP Prompt", "RP Calc", "RP Ratio", "RP Base", "RP Use", "RP LoRA", "RP Options", "RP Flip", "RP threshold",
     "FreeU Stages", "FreeU Schedule",
     "Mimic", "Separate Feature Channels", "Scaling Startpoint", "Variability Measure",  # Dynamic thresholding
