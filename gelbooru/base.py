5f17f19472e1f176cab5816b401421c5e211e97f
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2026-04-07 14:48:46 -0400

    query cache

--- gelbooru/base.py
+++ gelbooru/base.py
@@ -1,6 +1,6 @@
 import aiohttp
 import discord
-from typing import Dict, List
+from typing import Dict, List, Any
 from expiringdict import ExpiringDict, Union
 from redbot.core import commands
 from redbot.core.bot import Red, Config
@@ -15,6 +15,7 @@ class BooruBase(commands.Cog):
         self.session = aiohttp.ClientSession(headers=HEADERS)
         self.tag_cache: Dict[str, str] = {}
         self.image_cache: Dict[int, List[int]] = ExpiringDict(max_len=100, max_age_seconds=24*60*60)
+        self.query_cache: Dict[int, List[Dict[str, Any]] = ExpiringDict(max_len=100, max_age_seconds=24*60*60)
         self.config = Config.get_conf(self, identifier=62667275)
         self.config.register_global(tag_cache={})
 
