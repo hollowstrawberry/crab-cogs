6557e0c9f212233e47c51755f0e7a63bb893dbe0
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-04-12 16:03:26 -0400

    revamp logs cog with a view

--- logs/logs.py
+++ logs/logs.py
@@ -1,26 +1,11 @@
-import os
 import discord
 import aiofiles
-import aiofiles.os
 from typing import Optional
 from redbot.core import commands, Config
 from redbot.core.bot import Red
-from redbot.core.utils.views import SimpleMenu
-from redbot.core.data_manager import core_data_path
 
-LATEST_LOGS = os.path.join(core_data_path(), "logs/latest.log")
-MAX_PAGE_LENGTH = 1970
-
-
-async def get_logs_file():
-    if await aiofiles.os.path.exists(LATEST_LOGS):
-        return LATEST_LOGS
-    path = os.path.join(core_data_path(), "logs")
-    files = await aiofiles.os.listdir(path)
-    if not files:
-        return None
-    files.sort()
-    return os.path.join(path, files[-1])
+from logs.logs_view import LogsView
+from logs.utils import get_logs_file, MAX_PAGE_LENGTH, BACKTICK_PATTERN
 
 
 class Logs(commands.Cog):
@@ -30,21 +15,20 @@ class Logs(commands.Cog):
         super().__init__()
         self.bot = bot
         self.config = Config.get_conf(self, identifier=66677363)
-        self.config.register_global(private=True)
+        self.config.register_global()
 
 
     @commands.group(invoke_without_command=True) # type: ignore
     @commands.is_owner()
     async def logs(self, ctx: commands.Context, lines: Optional[int]):
-        """Sends the last n lines of the latest log file (default 100)."""
+        """Opens the logs view with the last n lines of the latest log file (default 100)."""
         try:
-            private = await self.config.private()
-            channel = ctx.channel if not private else (ctx.author.dm_channel or await ctx.author.create_dm())
             if not lines or lines < 0:
                 lines = 100
 
-            pages = []
-            if logs_file := await get_logs_file():
+            pages: list[str] = []
+            logs_file = await get_logs_file()
+            if logs_file:
                 async with aiofiles.open(logs_file, 'r', encoding="utf8") as f:
                     f_lines = await f.readlines()
                     result = [line.strip() for line in f_lines[-lines:]]
@@ -57,39 +41,24 @@ class Logs(commands.Cog):
                             page, result[-1] = "..." + result[-1][-MAX_PAGE_LENGTH:], result[-1][:-MAX_PAGE_LENGTH] + "...",
                         else:
                             break
-                    pages.append(f"```py\n{page.strip()}```")
+                    pages.append(f"```py\n{BACKTICK_PATTERN.sub('`', page).strip()}\n```")
 
             if not pages:
-                await channel.send("*Empty*")
-            elif len(pages) == 1:
-                await channel.send(content=pages[0])
+                await ctx.send("*No logs*")
             else:
-                pages.reverse()
-                for i in range(len(pages)):
-                    pages[i] += f"`Page {i+1}/{len(pages)}`"
-                ctx.message.channel = channel
-                ctx.message.guild = channel.guild
-                new_ctx: commands.Context = await self.bot.get_context(ctx.message)
-                await SimpleMenu(pages, timeout=3600, page_start=len(pages)-1).start(new_ctx)
+                embeds = []
+                for i, page in enumerate(reversed(pages)):
+                    embed = discord.Embed(description=page)
+                    if len(pages) > 1:
+                        embed.set_footer(text=f"Page {i+1}/{len(pages)}")
+                    embeds.append(embed)
+                view = LogsView(logs_file or "", embeds, self.bot)
+                view.message = await ctx.send(view=view)
+                if ctx.bot_permissions.manage_messages:
+                    try:
+                        await ctx.message.delete()
+                    except discord.NotFound:
+                        pass
 
         except Exception as ex:  # Since logs is an important command, all possible errors should be covered
             await ctx.send(f"{type(ex).__name__}: {ex}")
-
-
-    @logs.command(name="file")
-    async def logs_file(self, ctx: commands.Context):
-        """Sends the entire latest log file."""
-        private = await self.config.private()
-        channel = ctx.channel if not private else (ctx.author.dm_channel or await ctx.author.create_dm())
-        path = await get_logs_file()
-        if not path:
-            return await channel.send("No logs found.")
-        file = discord.File(path)
-        await channel.send(file=file)
-
-    @logs.command(name="private")
-    async def logs_private(self, ctx: commands.Context):
-        """Whether logs should be sent to your DMs."""
-        new_value = not await self.config.private()
-        await self.config.private.set(new_value)
-        await ctx.send("Logs will now be sent " + ("in your DMs" if new_value else "in the same channel"))
---
+++ logs/logs_view.py
@@ -0,0 +1,64 @@
+import discord
+from typing import Optional
+from discord.ui import View
+from redbot.core import commands
+from redbot.core.utils.views import SimpleMenu
+from redbot.core.bot import Red
+
+from imagescanner.constants import VIEW_TIMEOUT
+
+
+class LogsView(View):
+    def __init__(self, filepath: str, embeds: list[discord.Embed], bot: Red):
+        super().__init__(timeout=VIEW_TIMEOUT)
+        self.filepath = filepath
+        self.embeds = embeds
+        self.bot = bot
+        self.message: Optional[discord.Message] = None
+        self.logs_button = discord.ui.Button(emoji="📜", label="Logs", style=discord.ButtonStyle.blurple)
+        self.logs_button.callback = self.show_logs
+        self.file_button = discord.ui.Button(emoji="📁", label="File", style=discord.ButtonStyle.gray)
+        self.file_button.callback = self.show_file
+        self.save_button = discord.ui.Button(emoji="📨", label="Save", style=discord.ButtonStyle.gray)
+        self.save_button.callback = self.save_dm
+        self.add_item(self.logs_button)
+        self.add_item(self.file_button)
+        self.add_item(self.save_button)
+
+    async def check_owner(self, interaction: discord.Interaction):
+        if await self.bot.is_owner(interaction.user):
+            return True
+        else:
+            await interaction.response.send_message("You must be the bot owner to view the logs.", ephemeral=True)
+            return False
+
+    async def show_logs(self, interaction: discord.Interaction):
+        if not await self.check_owner(interaction):
+            return
+        ctx = await commands.Context.from_interaction(interaction)
+        await SimpleMenu(self.embeds, timeout=VIEW_TIMEOUT, page_start=len(self.embeds)-1).start(ctx, ephemeral=True)  # type: ignore
+
+    async def show_file(self, interaction: discord.Interaction):
+        if not await self.check_owner(interaction):
+            return
+        file = discord.File(self.filepath)
+        await interaction.response.send_message(file=file, ephemeral=True)
+
+    async def save_dm(self, interaction: discord.Interaction):
+        if not await self.check_owner(interaction):
+            return
+        file = discord.File(self.filepath)
+        try:
+            await interaction.user.send(file=file)
+        except discord.Forbidden:
+            await interaction.response.send_message("It appears you don't accept DMs, so I can't send you the logs file.", ephemeral=True)
+        else:
+            await interaction.response.send_message("Logs file sent to your DMs.", ephemeral=True)
+
+    async def on_timeout(self) -> None:
+        await super().on_timeout()
+        if self.message:
+            try:
+                await self.message.delete()
+            except (discord.NotFound, discord.Forbidden):
+                pass
