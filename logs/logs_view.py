8cff3c1281da26f3ff593bffdf4b48d90d2e4a70
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-04-14 05:19:50 -0400

    delete button for logs

--- logs/logs_view.py
+++ logs/logs_view.py
@@ -20,9 +20,12 @@ class LogsView(View):
         self.file_button.callback = self.show_file
         self.save_button = discord.ui.Button(emoji="📨", label="Save", style=discord.ButtonStyle.gray)
         self.save_button.callback = self.save_dm
+        self.delete_button = discord.ui.Button(emoji="✖️", style=discord.ButtonStyle.red)
+        self.delete_button.callback = self.delete
         self.add_item(self.logs_button)
         self.add_item(self.file_button)
         self.add_item(self.save_button)
+        self.add_item(self.delete_button)
 
     async def check_owner(self, interaction: discord.Interaction):
         if await self.bot.is_owner(interaction.user):
@@ -56,6 +59,17 @@ class LogsView(View):
             await interaction.response.send_message("It appears you don't accept DMs, so I can't send you the logs file.", ephemeral=True)
         else:
             await interaction.response.send_message("Logs file sent to your DMs.", ephemeral=True)
+
+    async def delete(self, interaction: discord.Interaction):
+        if not await self.check_owner(interaction):
+            return
+        self.message = None
+        self.stop()
+        if interaction.message:
+            try:
+                await interaction.message.delete()
+            except (discord.NotFound, discord.Forbidden):
+                pass
 
     async def on_timeout(self) -> None:
         await super().on_timeout()
