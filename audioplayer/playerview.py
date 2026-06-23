5ce47a9d7788338989ce47cee2a5da6e7af4436b
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-06-22 23:12:29 -0400

    audioplayer permissions fix

--- audioplayer/playerview.py
+++ audioplayer/playerview.py
@@ -3,6 +3,7 @@ import logging
 import discord
 import lavalink
 from copy import copy
+from typing import Optional
 from datetime import datetime
 from discord.ui import View
 from redbot.core import commands
@@ -26,82 +27,67 @@ class AudioPlayerView(View):
     @discord.ui.button(emoji="🇶", style=discord.ButtonStyle.grey)
     async def queue(self, inter: discord.Interaction, _):
         audio: Audio = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, "queue", ephemeral=True)
-        if not await self.can_run_command(ctx, "queue"):
-            await inter.response.send_message(ERROR_FORBIDDEN)
+        if not (ctx := await self.get_context(inter, "queue", ephemeral=True)):
             return
         try:
             await audio.command_queue(ctx)
         except Exception: # user-facing error
             log.error("queue button", exc_info=True)
-            if not inter.response.is_done():
-                await inter.response.send_message(ERROR_UNKNOWN)
+            await inter.followup.send(ERROR_UNKNOWN)
 
     @discord.ui.button(emoji="⏪", style=discord.ButtonStyle.grey)
     async def previous(self, inter: discord.Interaction, _):
         audio: Audio = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, "prev", ephemeral=False)
-        if not await self.can_run_command(ctx, "prev"):
-            await inter.response.send_message(ERROR_FORBIDDEN)
+        if not (ctx := await self.get_context(inter, "prev", ephemeral=False)):
             return
         try:
             await audio.command_prev(ctx)
         except Exception: # user-facing error
             log.error("previous button", exc_info=True)
-            if not inter.response.is_done():
-                await inter.response.send_message(ERROR_UNKNOWN)
+            await inter.followup.send(ERROR_UNKNOWN)
         else:
             await self.update_player(ctx, audio)
 
     @discord.ui.button(style=discord.ButtonStyle.grey)
     async def pause(self, inter: discord.Interaction, _):
         audio: Audio = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, "pause", ephemeral=True)
-        if not await self.can_run_command(ctx, "pause"):
-            await inter.response.send_message(ERROR_FORBIDDEN)
+        if not (ctx := await self.get_context(inter, "pause", ephemeral=True)):
             return
         try:
             await audio.command_pause(ctx)
         except Exception: # user-facing error
             log.error("pause button", exc_info=True)
-            if not inter.response.is_done():
-                await inter.response.send_message(ERROR_UNKNOWN)
+            await inter.followup.send(ERROR_UNKNOWN)
         else:
             await self.update_player(ctx, audio)
 
     @discord.ui.button(emoji="⏩", style=discord.ButtonStyle.grey)
     async def skip(self, inter: discord.Interaction, _):
         audio: Audio = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, "skip", ephemeral=False)
-        if not await self.can_run_command(ctx, "skip"):
-            await inter.response.send_message(ERROR_FORBIDDEN)
+        if not (ctx := await self.get_context(inter, "skip", ephemeral=False)):
             return
         try:
             await audio.command_skip(ctx)
         except Exception: # user-facing error
             log.error("skip button", exc_info=True)
-            if not inter.response.is_done():
-                await inter.response.send_message(ERROR_UNKNOWN)
+            await inter.followup.send(ERROR_UNKNOWN)
         else:
             await self.update_player(ctx, audio)
 
     @discord.ui.button(emoji="⏹️", style=discord.ButtonStyle.grey)
     async def stop(self, inter: discord.Interaction, _):
         audio: Audio = self.cog.bot.get_cog("Audio")
-        ctx = await self.get_context(inter, "stop", ephemeral=False)
-        if not await self.can_run_command(ctx, "stop"):
-            await inter.response.send_message(ERROR_FORBIDDEN)
+        if not (ctx := await self.get_context(inter, "stop", ephemeral=False)):
             return
         try:
             await audio.command_stop(ctx)
         except Exception: # user-facing error
             log.error("stop button", exc_info=True)
-            if not inter.response.is_done():
-                await inter.response.send_message(ERROR_UNKNOWN)
+            await inter.followup.send(ERROR_UNKNOWN)
         else:
             await self.update_player(ctx, audio)
 
-    async def get_context(self, inter: discord.Interaction, command_name: str, ephemeral: bool) -> commands.Context:
+    async def get_context(self, inter: discord.Interaction, command_name: str, ephemeral: bool) -> Optional[commands.Context]:
         # spoof a context where the user legitimately called the audio cog's command
         prefix = await self.cog.bot.get_prefix(self.message)
         prefix = prefix[0] if isinstance(prefix, list) else prefix
@@ -110,6 +96,13 @@ class AudioPlayerView(View):
         fake_message.content = prefix + command_name
         fake_message.author = inter.user
         ctx: commands.Context = await self.cog.bot.get_context(fake_message)
+        # permissions
+        try:
+            command = ctx.bot.get_command(command_name)
+            if not await command.can_run(ctx, check_all_parents=True, change_permission_state=False):
+                return None
+        except commands.CommandError:
+            return None
         # deferring allows me to only send followups, regular interaction responses cause weird edge cases with my hacky setup
         await inter.response.defer()
         # convert command responses into interaction responses
@@ -125,14 +118,6 @@ class AudioPlayerView(View):
             return await inter.followup.send(**new_kwargs)  # type: ignore
         ctx.send = types.MethodType(send, ctx)
         return ctx
-
-    async def can_run_command(self, ctx: commands.Context, command_name: str) -> bool:
-        command = ctx.bot.get_command(command_name)
-        try:
-            can = await command.can_run(ctx, check_all_parents=True, change_permission_state=False)
-        except commands.CommandError:
-            can = False
-        return can
     
     async def update_player(self, ctx: commands.Context, audio: Audio):
         assert ctx.guild is not None
