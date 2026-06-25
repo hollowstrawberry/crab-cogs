3c3dbdb37e649a27499c14fa8de89b4966617288
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-06-24 21:51:01 -0400

    Update playerview.py

--- audioplayer/playerview.py
+++ audioplayer/playerview.py
@@ -49,7 +49,7 @@ class AudioPlayerView(View):
             return
         try:
             if action == "seek":
-                await audio.command_seek(ctx, -10e12)
+                await audio.command_seek(ctx, -player.position // 1000)
             elif await audio.command_prev(ctx) is None: 
                 player.queue.insert(0, current_song)
         except Exception: # user-facing error
