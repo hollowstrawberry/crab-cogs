33f6101d59356282bd119782e3eba2cf975cb29a
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-06-24 20:57:15 -0400

    Update tts.py

--- tts/tts.py
+++ tts/tts.py
@@ -4,7 +4,7 @@ import logging
 import asyncio
 import lavalink
 from gtts import gTTS
-from copy import deepcopy
+from copy import copy, deepcopy
 from typing import Optional
 from googletrans import Translator
 from discord.ext import tasks
@@ -82,14 +82,14 @@ class TextToSpeech(Cog):
             return
 
         if player and player.current:
-            old_track = player.current
+            old_track = copy(player.current)
             old_timestamp = old_track.position
             player.queue.insert(0, old_track)
         new_track = load_result.tracks[0]
         new_track.requester = ctx.author  # type: ignore
         player.queue.insert(0, new_track)
         await player.play()
-        player.queue[0].start_timestamp = old_timestamp
+        old_track.start_timestamp = old_timestamp
 
         if ctx.interaction:
             await ctx.reply("🗣 Playing speech...")
