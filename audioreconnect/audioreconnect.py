737fcb5e4180a5da6b322e5ad32935fba4cb9278
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-07-02 21:28:26 -0400

    Update audioreconnect.py

--- audioreconnect/audioreconnect.py
+++ audioreconnect/audioreconnect.py
@@ -27,7 +27,7 @@ def pickle_track(track: lavalink.Track):
 
 
 async def neuter_persistent_queue(queue_api: QueueInterface):
-    async def dummy_fetch(self, *args):
+    async def dummy_fetch(self, *args, **kwargs):
         return []
     dummy = types.MethodType(dummy_fetch, queue_api)
     queue_api.fetch_all = dummy
