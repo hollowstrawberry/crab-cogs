2557eeee3d8f9c3761b247f5912848af66750717
hollowstrawberry <33796679+hollowstrawberry@users.noreply.github.com>
2026-07-17 21:38:55 -0400

    import fix

--- typingfix/__init__.py
+++ typingfix/__init__.py
@@ -3,5 +3,5 @@ from redbot.core.utils import get_end_user_data_statement
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
-async def setup(bot):
-    await bot.add_cog(TypingFix(bot))
+async def setup(_):
+    await bot.add_cog(TypingFix())
